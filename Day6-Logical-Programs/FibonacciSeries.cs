fd70e0eb0ebe969dc2e5c04799180c33878a8ed4
rinkuberde <rinkuberde@gmail.com>
2023-01-12 16:12:58 +0530

    [Rinku] - Fibonacci Series

---
+++ Day6-Logical-Programs/FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Logical_Programs
+{
+    internal class FibonacciSeries
+    {
+        public static void Fibonacci()
+        {
+            //variables
+            int fibonacci, previous_number = 0, current_number = previous_number + 1;
+           
+            //display initial numbers 1 and 2.
+            Console.Write("Fibonacci Series :" + previous_number + " " + current_number);
+            for (int i= 1; i <= 6; i++)
+            {
+                fibonacci = previous_number + current_number;
+                previous_number = current_number;
+                current_number = fibonacci;
+                Console.Write(" " + fibonacci);
+            }
+     
+        }
+    }
+}
+
--- Day6-Logical-Programs/Program.cs
+++ Day6-Logical-Programs/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical-Programs!!!");
+            Console.WriteLine("Welcome to Logical Programs!!!");
+            Console.WriteLine("Press 1. for Fibonacci Series.");
+            Console.WriteLine("Press 2. for Perfect number.");
+            Console.WriteLine("Press 3. for Prime number.");
+            Console.WriteLine("Press 4. for Reverse number.");
+            Console.WriteLine("Press 5. to Stimulate Stop watch.");
+            //user input
+            int selection = Convert.ToInt32(Console.ReadLine());
+            //validating input 
+            while (selection < 1 || selection > 6)
+            {
+                Console.WriteLine("Invalid input");
+                Console.WriteLine("Enter a proper selection");
+                selection = Convert.ToInt32(Console.ReadLine());
+            }
+            //switch case
+            switch (selection)
+            {
+                case 1:
+                    //Fibonacci Series
+                    FibonacciSeries.Fibonacci();                   
+                    break;
+            }
         }
     }
 }
