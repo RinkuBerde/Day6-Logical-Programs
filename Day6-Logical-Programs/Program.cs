2943c065c9e34b7f5fa8d7c61213b6a67b139efd
rinkuberde <rinkuberde@gmail.com>
2023-01-12 11:35:05 +0530

    [Rinku] - Welcome Message

---
+++ Day6-Logical-Programs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day6_Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical-Programs!!!");
+        }
+    }
+}
